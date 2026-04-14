07c7e56a9f5667f31a5470be88cc9b09367bc341
hamseungyeal <ham.seungyeal@gmail.com>
2026-04-14 23:40:15 +0900

    feat: add ngrok channel

--- src/api/client.js
+++ src/api/client.js
@@ -6,6 +6,7 @@ async function request(path, options = {}) {
     ...options,
     headers: {
       'Content-Type': 'application/json',
+      'ngrok-skip-browser-warning': 'true',
       ...(token ? { Authorization: `Bearer ${token}` } : {}),
       ...options.headers,
     },
